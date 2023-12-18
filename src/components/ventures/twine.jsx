21b049c797d86b4b3a3714b28dca56066525a0d0
LAPTOP-4ie <4iejohannes@gmail.com>
2023-12-18 21:05:03 +0100

    Added Twine Eagles Rest to my ventures

--- src/components/ventures/twine.jsx
+++ src/components/ventures/twine.jsx
@@ -1,24 +1,40 @@
 import React from 'react';
 
+const imagePaths = [
+    '/images/Twine Start.png',
+    '/images/Twine Old man.png',
+    '/images/Twine Inventory.png',
+    '/images/Twine Combat.png',
+];
+
 export const twine_layout = () => (
 
     <div className='venture-container'>
         <p>
-            One of my first forays into making games for myself was with a program 
-            called <a className="external-link" href="https://twinery.org/" target="_blank" rel="noopener noreferrer">Twine</a>.
+            I have always been interested in games and fantasy worlds. I wanted to take a stab at making something myself,
+            so I looked around for a piece of software that I could use to create a text adventure game.
+            The program I found is called <a className="external-link" href="https://twinery.org/" target="_blank" rel="noopener noreferrer">Twine</a>.
         </p>
         <br />
         <p>
-            ...
+            As stated on their webpage, it is a open-source tool for telling interactive, nonlinear stories. So I set about learning the software and
+            making a few small trial stories for myself using the SugarCube format. Eventually I put together a longer story called Eagles Rest. I could finally put some of the programming logic
+            I had learned at university to work in a way that I found entertaining.
         </p>
+        <br />
+        <p>
+            I eventually added a basic combat system, inventory, and managed to create a world that changed as you lived in it. It was a very satisfying project.
+            I will admit that it was never really wrapped up nicely, but it was a really fun learning experience. And who knows, maybe one day I will go back to it.
+        </p>
+        <br />
 
-        {/* <figure>
-            <img className='venture-image'
-                src="../images/Dynamo bridge cladding.png" 
-                alt="Dynamo bridge cladding"
-            />
-            <figcaption>Bridge cladding script</figcaption>
-        </figure> */}
+        <div className="bento-box">
+            {imagePaths.map((path, index) => (
+                <img key={index} src={process.env.PUBLIC_URL + path} alt={`Twine image ${index + 1}`}
+                />
+            ))}
+
+        </div>
 
 
     </div >
